b93ffda4a855df0f5fdddcc7b4caa7e60bf78bc2
Kim JaeHyung <you3192001@icloud.com>
2025-11-13 16:25:12 +0900

    [Feat] Collection SwapAt 구성및, Flutter의 제네릭

---
+++ day_08/lib/common/dart/extension/collection_extensions.dart
@@ -0,0 +1,48 @@
+// MARK: List
+import 'dart:collection';
+import 'dart:developer';
+
+extension ListSwapExtensions<T> on List<T> {
+  void swapAt(int from, int to) {
+    final temp = this[from];
+    this[from] = this[to];
+    this[to] = temp;
+  }
+}
+
+// MARK: Test
+// class TestModel {
+//   String name;
+
+//   TestModel({required this.name});
+// }
+
+// void main() {
+//   final test = [1, 2, 3, 4, 5];
+//   test.swapAt(3, 1);
+//   log(test.toString()); // [log] [1, 4, 3, 2, 5]
+
+//   // map
+//   final map = HashMap<String, TestModel>();
+//   map["toss"] = TestModel(name: "ASD");
+//   map["kakao"] = TestModel(name: "ASDKakao");
+//   map.putIfAbsent("kakao", () => TestModel(name: "ASDKakao2"));
+
+//   // 생성
+//   final list = [];
+//   final map2 = {};
+//   final set = {};
+// }
+
+// final class Results<T, F> {
+//   final T success;
+//   final F failure;
+
+//   Results({required this.success, required this.failure});
+// }
+
+// final class EmptyClass {}
+
+// Results TestResultFunc<T, F extends EmptyClass>(T success, F failure) {
+//   return Results(success: success, failure: failure);
+// }
